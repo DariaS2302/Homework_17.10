80ce5840fb1aaca4e2b6a61bda9b683030c3a399
Sarycheva Daria <d.saricheva@gmail.com>
2024-10-18 15:03:10 +0300

    add BotForTest

---
+++ src/test/java/tests/DemoqaFormTest.java
@@ -0,0 +1,76 @@
+package tests;
+
+
+import org.junit.jupiter.api.*;
+import pages.DemoqaFormPage;
+
+import static io.qameta.allure.Allure.step;
+
+@Tag("fullFormTest")
+@DisplayName("Проверка полного заполнения формы")
+
+public class DemoqaFormTest extends TestBase {
+
+    DemoqaFormPage demoqaFormPage = new DemoqaFormPage();
+
+    String firstName = "Daria",
+            lastName = "Sarycheva",
+            userEmail = "daria@test.com",
+            gender = "Female",
+            userNumber = "8900900999",
+            dayOfBirth = "23",
+            monthOfBirth = "February",
+            yearOfBirth = "1990",
+            subjects = "Art",
+            hobbies = "Sports",
+            pictureName = "tst.jpg",
+            address = "World",
+            state = "NCR",
+            city = "Del";
+
+    @Test
+    void fullFormTest() {
+     //  SelenideLogger.addListener("allure", new AllureSelenide());
+
+        step("Заполнение firstName", () ->
+                demoqaFormPage.setFirstName(firstName));
+
+        step("Заполнение lastName", () ->
+                demoqaFormPage.setLastName(lastName));
+
+        step("Заполнение userEmail", () ->
+                demoqaFormPage.setEmail(userEmail));
+        step("Заполнение gender", () ->
+                demoqaFormPage.setGender(gender));
+        step("Заполнение userNumber", () ->
+                demoqaFormPage.setUserNumber(userNumber));
+        step("Заполнение BirthDay", () ->
+                demoqaFormPage.setBirthDay(dayOfBirth, monthOfBirth, yearOfBirth));
+        step("Заполнение subjects", () ->
+                demoqaFormPage.setSubject(subjects));
+        step("Заполнение hobbies", () ->
+                demoqaFormPage.setHobby(hobbies));
+        step("Заполнение pictureName", () ->
+                demoqaFormPage.uploadPicture(pictureName));
+        step("Заполнение address", () ->
+                demoqaFormPage.setCurrentAddress(address));
+        step("Заполнение state", () ->
+                demoqaFormPage.setUserState(state));
+        step("Заполнение city", () ->
+                demoqaFormPage.setUserCity(city));
+        step("Нажать Submit", demoqaFormPage::clickSubmit);
+
+        step("Проверка заполненной формы", () -> {
+            demoqaFormPage.checkSuccessResult("Student Name", firstName + " " + lastName)
+                    .checkSuccessResult("Student Email", userEmail)
+                    .checkSuccessResult("Gender", gender)
+                    .checkSuccessResult("Mobile", userNumber)
+                    .checkSuccessResult("Date of Birth", dayOfBirth + " " + monthOfBirth + "," + yearOfBirth)
+                    .checkSuccessResult("Subjects", subjects)
+                    .checkSuccessResult("Hobbies", hobbies)
+                    .checkSuccessResult("Picture", pictureName)
+                    .checkSuccessResult("Address", address)
+                    .checkSuccessResult("State and City", state + " " + city);
+        });
+    }
+}
---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,63 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import pages.DemoqaFormPage;
+
+import java.util.Map;
+import java.util.Objects;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static io.qameta.allure.Allure.step;
+
+
+public class TestBase {
+    private final DemoqaFormPage demoqaFormPage = new DemoqaFormPage();
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browser = System.getProperty("browserNameAndVersion", "chrome");
+        Configuration.browserSize = System.getProperty("browserSize");
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.remote = System.getProperty("remoteURL");
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
+
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+    }
+
+    @BeforeEach
+    void openPage() {
+        step("Открываем форму", () -> {
+            demoqaFormPage.openPage();
+        });
+
+        step("Удаляем баннеры", () -> {
+            demoqaFormPage.removeBanner();
+        });
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Screenshot");
+        if (!Objects.equals(Configuration.browser, "firefox")) {
+            Attach.pageSource();
+            Attach.browserConsoleLogs();
+        }
+        Attach.addVideo();
+        closeWebDriver();
+    }
+}
